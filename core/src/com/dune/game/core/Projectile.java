1098e0a965ce23ac954315dfbeef379ac353d182
Leonid <kissmykrabeskill@gmail.com>
2020-05-25 19:12:53 +0500

    UpdateProjectile

--- core/src/com/dune/game/core/Projectile.java
+++ core/src/com/dune/game/core/Projectile.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
-<<<<<<< HEAD
 import com.badlogic.gdx.math.Vector2;
 
 public class Projectile extends GameObject implements Poolable {
@@ -46,43 +45,5 @@ public class Projectile extends GameObject implements Poolable {
         if (position.x < 0 || position.x > 1280 || position.y < 0 || position.y > 720) {
             deactivate();
         }
-=======
-import com.badlogic.gdx.math.Vector;
-import com.badlogic.gdx.math.Vector2;
-
-import java.awt.*;
-
-public class Projectile {
-    private Vector2 position;
-    private Vector2 velocity;
-    private TextureRegion textureRegion;
-
-    private float speedProjectile = 200f;
-
-    public Projectile(TextureAtlas atlas) {
-        this.textureRegion = new TextureRegion(atlas.findRegion("bullet"));
-    }
-
-    public void setup(Vector2 startPosition, float angle) {
-        velocity = new Vector2(speedProjectile * MathUtils.cosDeg(angle), speedProjectile * MathUtils.sinDeg(angle));
-        position = new Vector2(startPosition.x + (23 * MathUtils.cosDeg(angle)), startPosition.y - 3 + (23 * MathUtils.sinDeg(angle)));
-    }
-
-
-    public void update(float dt) {
-        position.mulAdd(velocity, dt);
-    }
-
-    public boolean checkBounds(Vector2 position){
-        return (position.x < 0 || position.y < 0 || position.x > 1280 || position.y > 720);
-    }
-
-    public TextureRegion getTextureRegion() {
-        return textureRegion;
-    }
-
-    public Vector2 getPosition() {
-        return position;
->>>>>>> master
     }
 }
